4e3e5535dd1c67b3ce1422c504328443c98a954d
DimaKutko <dimakutko410@gmail.com>
2021-11-10 18:38:14 +0200

    Init lesson2

---
+++ Lesson2/index.js
@@ -0,0 +1,10 @@
+// const http = require('http');
+
+// const server = http.createServer();
+// server.on('request', (req, res) => {
+//     res.end('Start server...');
+// });
+
+// server.listen(8000, () => {
+//     console.log('Hello from node js');
+// });
