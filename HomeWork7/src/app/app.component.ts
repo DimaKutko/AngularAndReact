e151abc72e116378f19b0afa97c1cc63f8919537
DimaKutko <dimakutko410@gmail.com>
2021-11-24 00:16:47 +0200

    Impl homework7

--- HomeWork7/src/app/app.component.ts
+++ HomeWork7/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IMachine } from './IMachine';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'HomeWork7';
+  percent: number = 0.8;
+  price: number = 0.50;
+  numbers: number[] = [100,101,102,103,104,105];
+  count: number = 0;
+  machine?: IMachine;
+
+  plus(){
+    this.count++;
+  }
+
+  car(){
+    this.machine = {
+      name: 'БелАЗ 75710',
+      url_photo: 'https://100.ks.ua/images/uploaded/5a8d847ec1df954d0080eb82ae66d8f5.jpg'
+    }
+  }
+
+  plane(){
+    this.machine = {
+      name: 'Ан-255',
+      url_photo: 'https://seanews.ru/wp-content/uploads/2020/04/an-225-mrio.jpg'
+    }
+  }
+
+  ship(){
+    this.machine = {
+      name: 'Knock Nevis',
+      url_photo: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/da/Knock_Nevis.jpg/300px-Knock_Nevis.jpg',
+    }
+  }
 }
